eff8986584950d425e911636e77e21a90c9462b6
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-01 14:53:50 -0600

    Add second post for palettes
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ app.get("/api/v1/palettes/:id", (req, res) => {
 });
 
 app.post('/api/v1/projects', (req, res) => {
-  const project_name = req.body.name
+  const project_name = req.body.name;
   if(!project_name) {
     res.status(422).json({error: `Project was not created. Please include a project name`})
   } else {
@@ -79,6 +79,35 @@ app.post('/api/v1/projects', (req, res) => {
         res.status(500).json({ error });
       })
   }
+});
+
+app.post('/api/v1/palettes', (req, res)=> {
+  const newPalette = req.body;
+  const requiredParameters = ['palette_name', 'color1', 'color2', 'color3', 'color4', 'color5', 'project_id'];
+
+  for(let requiredParameter of requiredParameters) {
+    if (!newPalette[requiredParameter]) {
+      res.status(422).json({ error: `Palette was not added. Please include a ${requiredParameter}`})
+    }
+  }
+  database('projects').where({ id: newPalette.project_id}).select('id')
+  .then(projectID => {
+    if(!projectID) {
+      res.status(404).json({error: `No project found with id of ${projectID}`});
+    } else {
+      database('palettes').insert(newPalette, 'id')
+      .then(newID => {
+        res.status(201).json({ id: newID[0] })
+      })
+      .catch(error => {
+        res.status(500).json({ error });
+      })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ error });
+  })
+
 })
 
 module.exports = app;
