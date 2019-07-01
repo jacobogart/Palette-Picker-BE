db171fec93cf89144a2f3e57012f83f9e1fc38a4
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-01 14:37:23 -0600

    Add get for one project
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,35 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
-const database = require('knex')(configuration);
-
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("../knexfile")[environment];
+const database = require("knex")(configuration);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send('HELLOO');
+app.get("/", (req, res) => {
+  res.status(200).send("HELLOO");
 });
 
-app.get('/api/v1/projects', (req, res) => {
-  database('projects').select()
-  .then(projects => {
-    res.status(200).json(projects)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+app.get("/api/v1/projects", (req, res) => {
+  database("projects")
+    .select()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
-app.get('/api/v1/palettes', (req, res) => {
-  database('palettes').select()
-  .then(palettes => {
-    res.status(200).json(palettes)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+app.get("/api/v1/palettes", (req, res) => {
+  database("palettes")
+    .select()
+    .then(palettes => {
+      res.status(200).json(palettes);
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
+app.get("/api/v1/projects/:id", (req, res) => {
+  const { id } = req.param;
+  database("projects")
+    .where({ id })
+    .select()
+    .then(project => {
+      if (project.length) {
+        res.status(200).json(project[0]);
+      } else {
+        res.status(404).json({ error: `No project found with id of ${id}` });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
 
 module.exports = app;
