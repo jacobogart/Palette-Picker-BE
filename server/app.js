c78ecdf5a5987e961eba13aeb6a361f90f9bc225
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-01 14:30:20 -0600

    Add one get for projects
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('HELLOO');
+});
+
+app.get('/api/v1/projects', (req, res) => {
+  database('projects').select()
+  .then(projects => {
+    res.status(200).json(projects)
+  })
+  .catch(error => {
+    res.status(500).json({ error })
+  })
+
 })
 
 
